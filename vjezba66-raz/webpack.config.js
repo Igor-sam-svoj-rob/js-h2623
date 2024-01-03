f590b21199211f523a66a47120b91467b5add8fc
Igor Jevremović <ijevremovic@yahoo.com>
2024-01-03 10:50:33 +0100

    Add files via upload

---
+++ vjezba66-raz/webpack.config.js
@@ -0,0 +1,36 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    compress: true,
+    hot: true,
+    open: true,
+    historyApiFallback: true,
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Webpack",
+      filename: "index.html",
+      template: "./src/index.html",
+    }),
+  ],
+};
